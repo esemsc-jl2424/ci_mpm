4d0acc43c9abc4a1567e6a0c3735fbdb6cc5d91d
acse-jyl130 <jiayu.li24@imperial.ac.uk>
2024-10-15 13:29:24 +0100

    fix

--- simple_functions/functions1.py
+++ simple_functions/functions1.py
@@ -1,5 +1,6 @@
 from functools import cache
-__all__ = ['my_sum', 'factorial']
+import numpy as np
+__all__ = ['my_sum', 'factorial', 'sin']
 
 
 def my_sum(iterable):
@@ -7,6 +8,10 @@ def my_sum(iterable):
     for i in iterable:
         tot += i
     return tot
+
+
+def sin(x):
+    return np.sin(x)
 
 
 @cache
